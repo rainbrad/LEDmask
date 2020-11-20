37f8fc7903341149d14e383186dbb314555e558d
Raine Bradley <42818130+rainbrad@users.noreply.github.com>
2020-11-19 20:19:33 -0500

    Update smile.ino

--- LEDmask/smile/smile.ino
+++ LEDmask/smile/smile.ino
@@ -1,4 +1,4 @@
-// (c) Michael Schoeffler 2017, http://www.mschoeffler.de
+
 #include "FastLED.h"
 
 #define DATA_PIN 6
@@ -10,7 +10,7 @@
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  delay(3000); // initial delay of a few seconds is recommended
+  delay(3000); // initial delay
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip); // initializes LED strip
   FastLED.setBrightness(BRIGHTNESS);// global brightness
 }
@@ -24,58 +24,20 @@ void showProgramCleanUp(long delayTime) {
   delay(delayTime);
 }
 
-// switches on all LEDs. Each LED is shown in random color.
-// numIterations: indicates how often LEDs are switched on in random colors
-// delayTime: indicates for how long LEDs are switched on.
-void showProgramRandom(int numIterations, long delayTime) {
-  for (int iteration = 0; iteration < numIterations; ++iteration) {
-    for (int i = 0; i < NUM_LEDS; ++i) {
-      leds[i] = CHSV(random8(), 255, 255); // hue, saturation, value
-    }
-    FastLED.show();
-    delay(delayTime);
-  }
-}
 
-// Shifts a single pixel from the start of strip to the end.
-// crgb: color of shifted pixel
-// delayTime: indicates how long the pixel is shown on each LED
-void showProgramShiftSinglePixel(CRGB crgb, long delayTime) {
-  for (int i = 0; i < NUM_LEDS; ++i) {
-    leds[i] = crgb;
-    FastLED.show();
-    delay(delayTime);
-    leds[i] = CRGB::Black;
-  }
-}
-
-// Shifts multiple pixel from the start of strip to the end. The color of each pixel is randomized.
-// delayTime: indicates how long the pixels are shown on each LED
-void showProgramShiftMultiPixel(long delayTime) {
-  for (int i = 0; i < NUM_LEDS; ++i) {
-    for (int j = i; j > 0; --j) {
-      leds[j] = leds[j - 1];
-    }
-    CRGB newPixel = CHSV(random8(), 255, 255);
-    leds[0] = newPixel;
-    FastLED.show();
-    delay(delayTime);
-  }
-}
-
-void showSmile(long delayTime) {
-  int b,c;
-  for (int i = (NUM_LEDS / 2) + 3, j = (NUM_LEDS / 2) - 5 ; i < 168; i += 8, j -= 8) {
-    leds[i] = CRGB::Red;
+void showSmile(long delayTime) {      
+  int b,c;    
+  for (int i = (NUM_LEDS / 2) + 3, j = (NUM_LEDS / 2) - 5 ; i < 168; i += 8, j -= 8) {    // Draws smile from middle, i designates LEDs going up the strip (pos. direction)
+    leds[i] = CRGB::Red;                                                                  // j designates LEDs going down the strip (neg. direction)
     leds[j] = CRGB::Red;
-    leds[j + 1] = CRGB::Red;
+    leds[j + 1] = CRGB::Red;  // gives thickness of two LEDs
     leds[i + 1] = CRGB::Red;
     FastLED.show();
     b = i;
     c = j;
   }
     b += 7;
-    c -= 7;
+    c -= 7;                 // incrementing LEDs to create curve of the smile
     leds[b] = CRGB::Red;
     leds[c] = CRGB::Red;
     leds[b + 1] = CRGB::Red;
