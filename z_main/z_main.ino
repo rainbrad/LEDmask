c9ee02c9c616a5a5b485f39776e16f41bada4023
Raine Bradley <42818130+rainbrad@users.noreply.github.com>
2021-04-13 13:40:29 -0400

    Movement and Scaling Update
    Added new demo as well as adjusted the parameters of arguments to fit updated functions for smiles.

--- z_main/z_main.ino
+++ z_main/z_main.ino
@@ -1,6 +1,6 @@
 // Raine Bradley
-#include "a_smiles.h"
-#include "b_transpose.h"
+#include "smile/a_smiles.h"
+
 
 void setup() {
   delay(3000); // initial delay
@@ -10,28 +10,53 @@ void setup() {
 }
 
 void playDemo(long showTime, long cleanTime) {        //showTime: seconds each display will be shown (1000 = 1 second)        cleanTime: time between switching displays  smiles::smileSetup();
-  showSmile_1(0);
+  showSmile_1(6,3,0);
   delay(showTime);
   showProgramCleanUp(cleanTime);
-  showSmile_2(0);
+  showSmile_2(6,3,0);
   delay(showTime);
   showProgramCleanUp(cleanTime);
-  showSmile_3();
+  showSmile_3(5, 0, 0);
   delay(showTime);
   showProgramCleanUp(cleanTime);
   showSmile_4();
   delay(showTime);
   showProgramCleanUp(cleanTime);
-  showSmile_5full(1500, 10, 5);
+  showSmile_5full(1500,5);
+  delay(showTime);
+  showProgramCleanUp(cleanTime);
+  showSmile_6(6, 3, 0);
   delay(showTime);
   showProgramCleanUp(cleanTime);
 
 }
 
+void movementDemo(long showTime, long cleanTime) {
+  showSmile_1(6,3,0);
+  delay(showTime);
+  showProgramCleanUp(cleanTime);
+  showSmile_1(4,1,5);
+  delay(showTime);
+  showProgramCleanUp(cleanTime);
+  showSmile_1(2,4,10);
+  delay(showTime);
+  showProgramCleanUp(cleanTime);
+  showSmile_1(10,3,0);
+  delay(showTime);
+  showProgramCleanUp(cleanTime);
+}
+
 // main program
 void loop() {
   //playDemo(5000, 300);
+  //showProgramCleanUp(100);
+  showSmile_3(5, 1, -3);
+  delay(2500);
   showProgramCleanUp(100);
-  leds[transposeHor(10, -1)] = CRGB::Red;
-  FastLED.show();
+  showSmile_6(6,3,0);
+  delay(2500);
+  showProgramCleanUp(100);
+  movementDemo(2500,300);
+  showProgramCleanUp(100);
+  
 }
